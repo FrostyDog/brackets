337612af4cc196744f204e82e43af7cecd7f2203
Alex Gubskyy <aleks.gubskyy@gmail.com>
2020-03-21 00:51:44 +0100

    fix: works with every test

--- src/index.js
+++ src/index.js
@@ -1,81 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  let brArray = str.split("")
-  let stack = []
+    let brArray = str.split("")
+    let stack = []
 
-  let bracketsOptions = bracketsConfig.map(el => el.join("")).join("")
+    let bracketsOptions = bracketsConfig.map(el => el.join("")).join("")
+    if (str.length % 2 !== 0) { return false }
+    brArray.forEach(el => {
+        for (let n = 0; n < bracketsOptions.length; n += 2) {
 
-  brArray.forEach(el => {
-      for (let n = 0; n < bracketsOptions.length; n += 2) {
-          let m = n + 1
-          if (el == bracketsOptions[m] && bracketsOptions[n] == bracketsOptions[m] && bracketsOptions[n] == stack[(stack.length - 1)]) {
-              stack.pop()
-              break;
-          }
+            let m = n + 1
 
-          if (el == bracketsOptions[n]) {
-              stack.push(el)
-              break;
-          } else if (el == bracketsOptions[m]) {
-              if (bracketsOptions[n] == stack[(stack.length - 1)]) {
-                  stack.pop()
-                  break
-              } else {
-                  return false
-              }
-          } else {
-              return false
-          }
+            if (el == bracketsOptions[m]
+                && bracketsOptions[n] == bracketsOptions[m]
+                && bracketsOptions[n] == stack[stack.length - 1]) {
+                stack.pop()
+            } else if (el == bracketsOptions[n]
+                && bracketsOptions[n] == bracketsOptions[m]
+                && bracketsOptions[n] !== stack[stack.length - 1]) {
+                stack.push(el)
+            }
 
-      }
-  })
+            if (el == bracketsOptions[n] && bracketsOptions[n] !== bracketsOptions[m]) {
+                stack.push(el)
+            } else if (el == bracketsOptions[m] && bracketsOptions[n] !== bracketsOptions[m]) {
+                if (bracketsOptions[n] == stack[stack.length - 1]) {
+                    stack.pop()
+                } else {
+                    return false
+                }
+            }
+        }
+    })
 
-  return (stack.length == 0)
-
+    return (stack.length == 0)
 }
-
-
-
-  // ________________________________________
-
-//   function checkx(str, bracketsConfig) {
-//     let brArray = str.split("")
-//     let stack = []
-//     console.log(str)
-//     console.log(bracketsConfig)
-  
-//     // do {
-//     //   bracketsConfig.push([0, 0])
-//     // }
-//     // while (bracketsConfig.length < 4)
-//     let bracketsOptions = bracketsConfig.map(el => el.join("")).join("")
-//     brArray.forEach(el => {
-//   for (let n = 0; n < bracketsOptions.length; n += 2) {
-//       let m = n + 1
-//       if (el == bracketsOptions[m] && bracketsOptions[n] == bracketsOptions[m] && bracketsOptions[n] == stack[(stack.length - 1)]) {
-//         stack.pop()
-//         break;
-//       }
-      
-//       if (el == bracketsOptions[n]) {
-//           stack.push(el)
-//           break;
-//       } else if (el == bracketsOptions[m]) {
-//           if (bracketsOptions[n] == stack[(stack.length - 1)]) {
-//                   stack.pop()
-//                   break
-//               } else {console.log(false)}
-//           }
-//           else {
-//               console.log(false)
-//           }
-  
-//       }})
-  
-//       console.log(stack.length == 0)
-  
-//     }
-  
-
-
-
-// checkx("[]][[]", [ [ '(', ')' ], [ '[', ']' ], [ '{', '}' ] ])
